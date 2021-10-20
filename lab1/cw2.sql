6f4f7a1c7c4dfdbc53124d9250e5d081953d59e9
maati01 <mateuszpow7@gmail.com>
2021-10-20 21:48:40 +0200

    fix lines

--- lab1/cw2.sql
+++ lab1/cw2.sql
@@ -1,10 +1,14 @@
-/*1 */select title from title order by title
+/*1 */
+select title from title order by title
 
-/*2 */select distinct member_no, isbn, fine_assessed from loanhist where fine_assessed != 0
+/*2 */
+select distinct member_no, isbn, fine_assessed from loanhist where fine_assessed != 0
 select distinct member_no, isbn, fine_assessed, fine_assessed*2 as "double fine" from loanhist where fine_assessed != 0
 
-/*3 */ select firstname + ' ' +  middleinitial + ' ' + lastname from member where lastname = 'Anderson'
+/*3 */
+select firstname + ' ' +  middleinitial + ' ' + lastname from member where lastname = 'Anderson'
 select firstname + ' ' +  middleinitial + ' ' + lastname as 'email_name' from member where lastname = 'Anderson'
 select lower(firstname +  middleinitial + substring(lastname,1,2)) as 'email_name' from member where lastname = 'Anderson'
 
-/*4 */select 'The title is: ' + title + ', title number ' + trim(str(title_no)) from title
+/*4 */
+select 'The title is: ' + title + ', title number ' + trim(str(title_no)) from title
